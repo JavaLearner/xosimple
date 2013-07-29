09b8802e1965cdc0f222ca43c2f5e73f2408be03
JavaLearner <slavapv2007@gmail.com>
2013-07-29 06:27:01 +0300

    add new changes for compile in terminal

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
-javac -d out src/com/game/xo/common/*.java
 javac -d out src/com/game/xo/players/*.java
+javac -d out src/com/game/xo/common/LogicAlgorithm.java
+
+javac -sourcepath ./src -d out src/com/game/xo/common/CreateGame.java
 javac -sourcepath ./src -d out src/com/game/xo/main/*.java
 
 java -classpath ./out com.game.xo.main.Main

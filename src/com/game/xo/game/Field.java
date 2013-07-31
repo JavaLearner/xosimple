923bcb3183628f974db0d53270996f91ceb1bade
JavaLearner <slavapv2007@gmail.com>
2013-08-01 00:20:25 +0300

    Global changes !!!!!!!

--- src/com/game/xo/game/Field.java
+++ src/com/game/xo/game/Field.java
@@ -7,15 +7,11 @@ import java.util.Scanner;
 public class Field {
     private static final int AMOUNT_CELLS = 3;
     private char gameField[][] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-    public final char EMPTY_CELL = ' ';
-    public final char SYMBOL_X = 'x';
-    public final char SYMBOL_0 = '0';
-    private int globalStepCount;
-    private int axisX;
-    private int axisY;
 
-    private Scanner scanner = new Scanner(System.in);
-    private SearchWinner searchWinner = new SearchWinner();
+
+
+    //private Scanner scanner = new Scanner(System.in);
+    //private SearchWinner searchWinner = new SearchWinner();
 
     public Field() {
         globalStepCount = 0;
@@ -50,57 +46,16 @@ public class Field {
 
     }
 
-    public void gameMovies(Player player, boolean flagError) {
-        while (flagError && !player.getYouWin()) {
-            getPlayerStep();
-            try {
-                if ((player.getPlayerSymbol() == SYMBOL_X) && (gameField[axisX][axisY] == EMPTY_CELL)) {
-                    flagError = checkWin(player, flagError);
-                } else {
-                    if (gameField[axisX][axisY] == EMPTY_CELL) {
-                        flagError = checkWin(player, flagError);
-                    }
-                }
-
-            } catch (ArrayIndexOutOfBoundsException e)
-
-            {
-                System.out.println("Your coordinates invalid.\nPlease enter correct coordinates.");
-                displayField();
-                flagError = true;
-            }
-        }
-        flagError = checkWin(player, flagError);
-
+    public void updateField() {
 
     }
 
-    private void getPlayerStep() {
-        System.out.print("Enter coordinate (x, ): ");
-        axisX = scanner.nextInt();
-        System.out.print("Enter coordinate ( ,y): ");
-        axisY = scanner.nextInt();
-    }
-
-    private boolean checkWin(Player player, boolean flagError) {
-        if (!searchWinner.searchWinner(gameField, player.getPlayerSymbol())) {
-            gameField[axisX][axisY] = player.getPlayerSymbol();
-            flagError = updates(flagError, player);
-        } else {
-            System.out.println("\n" + player.getName() + " you win!!!\n");
-            player.setYouWin(true);
-            flagError = false;
-        }
-        return flagError;
-    }
-
-    private boolean updates(boolean flagError, Player player) {
-        if (flagError) {
-            globalStepCount++;
-            player.setPlayerSteps(globalStepCount);
-        }
-        flagError = false;
-        return flagError;
+    public char getFieldCell(int axisX, int axisY) {
+        return gameField[axisX][axisY];
 
     }
+
+
+
+
 }
--- src/com/game/xo/game/Game.java
+++ src/com/game/xo/game/Game.java
@@ -5,13 +5,19 @@ import com.game.xo.players.*;
 import java.util.Scanner;
 
 public class Game {
-    public static final int MAX_STEPS = 9;
+    public final int MAX_STEPS = 9;
     private final char HUMAN_PLAY = 'h';
     private final char COMPUTER_PLAY = 'c';
-
-
-
-    private Scanner scanner = new Scanner(System.in);
+    public final char EMPTY_CELL = ' ';
+    public final char SYMBOL_X = 'x';
+    public final char SYMBOL_0 = '0';
+    private int globalStepCount;
+    private int axisX;
+    private int axisY;
+    private int findWinner;
+    private static final int WINS = 3;
+    private Scanner in = new Scanner(System.in);
+    private Field field;
 
     public void Game() {
         boolean flagError = true;//false when no errors
@@ -21,7 +27,6 @@ public class Game {
         String chosenSymbol;
         String tempOption;
         char chosenOption;
-        Field field;
 
         while (endOfGameFlag) {
             field = new Field();
@@ -29,22 +34,23 @@ public class Game {
             viewMessage("Field start.\n");
 
             viewMessage("Enter first name: ");
-            namePlayer = scanner.nextLine();
+            namePlayer = in.nextLine();
             Player firstPlayer = new Human(namePlayer);
             Player secondPlayer = new Player();
 
             //check creation second player????????
             do {
                 viewMessage("You play with human or computer? h/c: ");
-                tempOption = scanner.nextLine();
+                tempOption = in.nextLine();
                 chosenOption = whatChosen(tempOption);
                 switch (chosenOption) {
                     case COMPUTER_PLAY:
                         secondPlayer = new Computer();
+
                         break;
                     case HUMAN_PLAY:
                         viewMessage("Enter second name: ");
-                        namePlayer = scanner.nextLine();
+                        namePlayer = in.nextLine();
                         secondPlayer = new Human(namePlayer);
                         break;
                     default:
@@ -54,10 +60,11 @@ public class Game {
                 }
             } while (!flagError);
             field.displayField();
+            flagError = true;
 
             do {
                 viewMessage(firstPlayer.getName() + "\nChoose your symbol x or 0 : ");
-                chosenSymbol = scanner.nextLine();
+                chosenSymbol = in.nextLine();
                 switch (chosenSymbol.charAt(0)) {
                     case '0':
                         setSymbol(firstPlayer, secondPlayer, field);
@@ -73,7 +80,7 @@ public class Game {
             } while (!flagError);
             while (!firstPlayer.getYouWin() && !secondPlayer.getYouWin() && secondPlayer.getPlayerSteps() < MAX_STEPS) {
                 viewMessage(firstPlayer.getName() + " your turn. Your symbol: " + firstPlayer.getPlayerSymbol());
-                field.gameMovies(firstPlayer, flagError);
+                gameMovies(firstPlayer, flagError);
                 field.displayField();
                 if (firstPlayer.getYouWin()) {
                     break;
@@ -89,7 +96,7 @@ public class Game {
             }
 
             viewMessage("Start the new game? y/n: ");
-            continueGame = scanner.nextLine();
+            continueGame = in.nextLine();
 
             flagError = false;
             while (!flagError) {
@@ -109,7 +116,62 @@ public class Game {
         }
 
         viewMessage("\nEnd of game...");
-        scanner.nextLine();
+        in.nextLine();
+    }
+
+    public void gameMovies(Player player, boolean flagError) {
+        while (flagError && !player.getYouWin()) {
+            getPlayerStep();
+            try {
+                if ((player.getPlayerSymbol() == SYMBOL_X) && (field.getFieldCell(axisX, axisY) == EMPTY_CELL)) {
+                    flagError = checkWin(player, flagError);
+                } else {
+                    if (field.getFieldCell(axisX, axisY) == EMPTY_CELL) {
+                        flagError = checkWin(player, flagError);
+                    }
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e)
+
+            {
+                System.out.println("Your coordinates invalid.\nPlease enter correct coordinates.");
+                field.displayField();
+                flagError = true;
+            }
+        }
+        flagError = checkWin(player, flagError);
+
+
+    }
+
+    //add - when play computer
+    private void getPlayerStep() {
+        System.out.print("Enter coordinate (x, ): ");
+        axisX = in.nextInt();
+        System.out.print("Enter coordinate ( ,y): ");
+        axisY = in.nextInt();
+    }
+
+    private boolean checkWin(Player player, boolean flagError) {
+        if (!searchWinner.searchWinner(gameField, player.getPlayerSymbol())) {
+            gameField[axisX][axisY] = player.getPlayerSymbol();
+            flagError = updates(flagError, player);
+        } else {
+            System.out.println("\n" + player.getName() + " you win!!!\n");
+            player.setYouWin(true);
+            flagError = false;
+        }
+        return flagError;
+    }
+
+    private boolean updates(boolean flagError, Player player) {
+        if (flagError) {
+            globalStepCount++;
+            player.setPlayerSteps(globalStepCount);
+        }
+        flagError = false;
+        return flagError;
+
     }
 
     private void setSymbol(Player player1, Player player2, Field newField) {
@@ -132,4 +194,72 @@ public class Game {
         }
         return 'e';
     }
+
+    public boolean searchWinner(char symbol) {
+        if (rowWinner(symbol) || columnWinner(symbol) || diagonalWinner(symbol)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /*search winner in rows*/
+    private boolean rowWinner(char symbol) {
+
+        for (int i = 0; i < tempField.length; i++) {
+            findWinner = rowWinnerSub(i, tempField.length, symbol);
+            if (findWinner == WINS) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int rowWinnerSub(int axisX, int size, char symbol) {
+        int sum = 0;
+        for (int j = 0; j < size; j++) {
+            if (field.getFieldCell(axisX, j) == symbol) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    /*search winner in columns*/
+    private boolean columnWinner(char symbol) {
+        for (int i = 0; i < tempField.length; i++) {
+            findWinner = columnWinnerSub(i, tempField.length, symbol);
+            if (findWinner == WINS) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int columnWinnerSub(int axisY, int size, char symbol) {
+        int sum = 0;
+        for (int j = 0; j < size; j++) {
+            if (tempField[j][axisY] == symbol) {
+                sum++;
+            }
+        }
+        return sum;
+    }
+
+    /*search winner in diagonals*/
+    private boolean diagonalWinner(char symbol) {
+        int i, sum = 0;
+        for (i = 0; i < tempField.length; i++) {
+            if (tempField[i][i] == symbol) {
+                sum++;
+            }
+        }
+        i = 1;
+        if (sum == WINS || tempField[i][i] == symbol && tempField[i + 1][i - 1] == symbol && tempField[i - 1][i + 1] == symbol) {
+            return true;
+        }
+        return false;
+
+    }
+
 }

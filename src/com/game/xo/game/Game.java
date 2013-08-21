a2460f6e9ec997e7ec49325c7279eca5d01d9123
JavaLearner <slavapv2007@gmail.com>
2013-08-22 02:03:57 +0300

    update class Game

--- src/com/game/xo/game/Game.java
+++ src/com/game/xo/game/Game.java
@@ -3,13 +3,16 @@ package com.game.xo.game;
 import com.game.xo.choice.ChoiceSymbol;
 import com.game.xo.display.IDisplay;
 import com.game.xo.field.IField;
+import com.game.xo.input.InputDataNumber;
 import com.game.xo.input.InputDataString;
+import com.game.xo.players.Human;
 import com.game.xo.players.Player;
 
 public class Game implements IGame {
     private Player player1;
     private Player player2;
-    private InputDataString inputDataString = new InputDataString();
+    private InputDataString inputDataString;
+    private InputDataNumber inputDataNumber;
     private IDisplay display;
     private IField field;
     private boolean endOfGame;
@@ -19,26 +22,38 @@ public class Game implements IGame {
         this.player2 = player2;
         this.display = display;
         this.field = field;
+        inputDataString = new InputDataString();
+        inputDataNumber = new InputDataNumber();
     }
 
     public void startGame() {
         ChoiceSymbol choiceSymbol = new ChoiceSymbol();
 
-//        do{
-        display.displayMessage("Game start.\n");
-        choiceSymbol.chooseSymbol(player1, player2);
-        display.displayMessage(player1.getName() + " your symbol : " + player1.getPlayerSymbol() + "\n");
-        display.displayMessage(player2.getName() + " your symbol : " + player2.getPlayerSymbol() + "\n");
-        display.displayMessage("\n");
-        field.displayField();
-//            gameMoves(player1, player2);
-//        }while(endOfGame);
+        do {
+            display.displayMessage("Game start.\n");
+            choiceSymbol.chooseSymbol(player1, player2);
+            display.displayMessage(player1.getName() + " your symbol : " + player1.getPlayerSymbol() + "\n");
+            display.displayMessage(player2.getName() + " your symbol : " + player2.getPlayerSymbol() + "\n");
+            display.displayMessage("\n");
+            field.displayField();
+            gameMoves(player1, player2);
+        } while (endOfGame);
 
 
     }
 
-//    private void gameMoves(Player player1, Player player2) {
-//
-//    }
+    private void gameMoves(Player player1, Player player2) {
+        if (player1 instanceof Human) {
+            boolean exceptionFlag = getCoordinates(player1);
+        }
+    }
+    private boolean getCoordinates(Player player) {
+        display.displayMessage("Please enter your coordinate (x,): ");
+        int inputX = inputDataNumber.getNumber();
+        display.displayMessage("Please enter your coordinate (,y): ");
+        int inputY = inputDataNumber.getNumber();
+        boolean exceptionFlag = field.setGameField(inputX, inputY, player.getPlayerSymbol());
+        return  exceptionFlag;
+    }
 
 }
